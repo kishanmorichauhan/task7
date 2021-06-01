48c5f736c56cda9653e58907fe7da423ee29c8cb
kishan morichauhan <kishanmorichauhan@gmail.com>
2021-06-01 23:04:37 +0530

    -

---
+++ app/src/main/java/com/example/task7/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.task7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SliderView sliderView;
+    int[] images = {R.drawable.image1,
+                    R.drawable.image2,
+                    R.drawable.image3,
+                    R.drawable.image4,
+                    R.drawable.image5,
+                    R.drawable.image6};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sliderView = findViewById(R.id.imageSlider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+    }
+}
